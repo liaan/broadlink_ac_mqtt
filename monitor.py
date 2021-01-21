96bee6f41054213fd777d42ba74f669681df4f53
Karman de Lange <liaan@vpx.co.za>
2021-01-21 06:30:43 +0000

    dunno

--- monitor.py
+++ monitor.py
@@ -8,7 +8,7 @@ import argparse
 import time
 import broadlink_ac_mqtt.AcToMqtt as AcToMqtt
 import broadlink_ac_mqtt.classes.broadlink.ac_db as ac_db_version
-import broadlink_ac_mqtt.classes.broadlink.ac_db as ac_db2
+
 import signal
 import traceback
 
@@ -128,10 +128,12 @@ def touch_pid_file():
 			f.write("%s,%s" % (os. getpid() ,pid_last_update))	
 
 def check_if_running():
+	
 	##Check if there is pid, if there is, then check if valid or stale .. probably should add process id for race conditions but damn, this is a simple scripte.....
-	if os.path.isfile(pidfile):
+	if os.path.isfile(pidfile):	
 
 		logger.debug("%s already exists, checking if stale" % pidfile)
+		
 		##Check if stale
 		f = open(pidfile, 'r') 
 		if f.mode =="r":
@@ -140,12 +142,12 @@ def check_if_running():
 			
 			##Stale, so go ahead
 			if (1 in contents and (float(contents[1])+ pid_stale_time) < time.time()):
-					logger.info("Pid is stale, so we'll just overwrite it go on")								
-				
-			else:
-				logger.debug("Pid is still valid, so exit")												
+				logger.info("Pid is stale, so we'll just overwrite it go on")								
+					
+			else:				
+				logger.info("Pid is still valid, so exit")																
 				sys.exit()
-	
+			
 	##Write current time
 	touch_pid_file()
 				
@@ -196,175 +198,177 @@ def init_signal():
 #################  Main startup ####################
 				
 def start():
-		##Handle signal
-		init_signal()
-		##Make sure not already running		
-		stop_if_already_running()		
-		##Just some defaults
-		##Defaults		
-		global AC	
-		devices = {}			 
-		
-				
-		# Argument parsing
-		parser = argparse.ArgumentParser(		
-			description='Aircon To MQTT v%s : Mqtt publisher of Duhnham Bush on the Pi.' % softwareversion			
-		)
-		
-		##HomeAssistant stuff
-		parser.add_argument("-Hd", "--dumphaconfig",help="Dump the devices as a HA manual config entry",action="store_true",default=False)
-		parser.add_argument("-Hat", "--mqtt_auto_discovery_topic", help="If specified, will Send the MQTT autodiscovery config for all devices to topic")
-		parser.add_argument("-b", "--background", help="Run in background",action="store_true",default=False)
-		##Config helpers
-		parser.add_argument("-S", "--discoverdump", help="Discover devices and dump config",action="store_true",default=False)
-		
-		#parser.add_argument("-dh", "--devicehost", help='Aircon Host IP, Default: %s ' % ac_host)
-		#parser.add_argument("-dm", "--devicemac", help="Ac Mac Address, Default:  %s" % ac_mac)
-		##MQTT stuff
-		parser.add_argument("-ms", "--mqttserver", help='Mqtt Server, Default:')
-		parser.add_argument("-mp", "--mqttport", help="Mqtt Port" )
-		parser.add_argument("-mU", "--mqttuser", help="Mqtt User" )
-		parser.add_argument("-mP", "--mqttpassword", help="Mqtt Password" )
-		
-		##Generic
-		parser.add_argument("-s", "--discover", help="Discover devices",action="store_true",default=False)
-		parser.add_argument("-d", "--debug", help="set logging level to debug",action="store_true",default=False)
-		parser.add_argument("-v", "--version", help="Print Verions",action="store_true")
-		parser.add_argument("-dir", "--data_dir", help="Data Folder -- Default to folder script is located", default=False)
-		parser.add_argument("-c", "--config", help="Config file path -- Default to folder script is located + 'config.yml'", default=False)
-		parser.add_argument("-l", "--logfile", help="Logfile path -- Default to logs folder script is located", default=False)
-				
-		##Parse args
-		args = parser.parse_args()
-		
-		##Set the base path, if set use it, otherwise default to running folder
-		if args.data_dir:
-			if os.path.exists(args.data_dir):
-				data_dir = args.data_dir
-			else:
-				print ("Path Not found for Datadir: %s" % (args.data_dir))
-				sys.exit()
+	
+	##Handle signal
+	init_signal()
+	##Make sure not already running		
+	stop_if_already_running()		
+	
+	##Just some defaults
+	##Defaults		
+	global AC	
+	devices = {}			 
+	
+	
+	# Argument parsing
+	parser = argparse.ArgumentParser(		
+		description='Aircon To MQTT v%s : Mqtt publisher of Duhnham Bush on the Pi.' % softwareversion			
+	)
+	
+	##HomeAssistant stuff
+	parser.add_argument("-Hd", "--dumphaconfig",help="Dump the devices as a HA manual config entry",action="store_true",default=False)
+	parser.add_argument("-Hat", "--mqtt_auto_discovery_topic", help="If specified, will Send the MQTT autodiscovery config for all devices to topic")
+	parser.add_argument("-b", "--background", help="Run in background",action="store_true",default=False)
+	##Config helpers
+	parser.add_argument("-S", "--discoverdump", help="Discover devices and dump config",action="store_true",default=False)
+	
+	#parser.add_argument("-dh", "--devicehost", help='Aircon Host IP, Default: %s ' % ac_host)
+	#parser.add_argument("-dm", "--devicemac", help="Ac Mac Address, Default:  %s" % ac_mac)
+	##MQTT stuff
+	parser.add_argument("-ms", "--mqttserver", help='Mqtt Server, Default:')
+	parser.add_argument("-mp", "--mqttport", help="Mqtt Port" )
+	parser.add_argument("-mU", "--mqttuser", help="Mqtt User" )
+	parser.add_argument("-mP", "--mqttpassword", help="Mqtt Password" )
+	
+	##Generic
+	parser.add_argument("-s", "--discover", help="Discover devices",action="store_true",default=False)
+	parser.add_argument("-d", "--debug", help="set logging level to debug",action="store_true",default=False)
+	parser.add_argument("-v", "--version", help="Print Verions",action="store_true")
+	parser.add_argument("-dir", "--data_dir", help="Data Folder -- Default to folder script is located", default=False)
+	parser.add_argument("-c", "--config", help="Config file path -- Default to folder script is located + 'config.yml'", default=False)
+	parser.add_argument("-l", "--logfile", help="Logfile path -- Default to logs folder script is located", default=False)
+			
+	##Parse args
+	args = parser.parse_args()
+	
+	##Set the base path, if set use it, otherwise default to running folder
+	if args.data_dir:
+		if os.path.exists(args.data_dir):
+			data_dir = args.data_dir
 		else:
-			data_dir = os.path.dirname(os.path.realpath(__file__))
-			
-		##Config File
-		if args.config:
-			if os.path.exists(args.config):
-				config_file_path = args.config
-			else:
-				print ("Config file not found: %s" % (args.config))
-				sys.exit()
-			 
+			print ("Path Not found for Datadir: %s" % (args.data_dir))
+			sys.exit()
+	else:
+		data_dir = os.path.dirname(os.path.realpath(__file__))
+		
+	##Config File
+	if args.config:
+		if os.path.exists(args.config):
+			config_file_path = args.config
 		else:
-			if os.path.exists(data_dir+'/settings/config.yml'):
-				config_file_path = data_dir+'/settings/config.yml'
-			else:
-				config_file_path = data_dir+'/config.yml'
+			print ("Config file not found: %s" % (args.config))
+			sys.exit()
 			
+	else:
+		if os.path.exists(data_dir+'/settings/config.yml'):
+			config_file_path = data_dir+'/settings/config.yml'
+		else:
+			config_file_path = data_dir+'/config.yml'
 		
-		##Config File
-		if args.logfile:			
-			log_file_path = args.config			 
-		else:			
-			log_file_path = os.path.dirname(os.path.realpath(__file__))+'/log/out.log'
-			
-		log_level = logging.DEBUG if args.debug else logging.INFO
-		init_logging(log_level,log_file_path)
+	
+	##Config File
+	if args.logfile:			
+		log_file_path = args.config			 
+	else:			
+		log_file_path = os.path.dirname(os.path.realpath(__file__))+'/log/out.log'
 		
-		logger.debug("%s v%s is starting up" % (__file__, softwareversion))
-		logLevel = {0: 'NOTSET', 10: 'DEBUG', 20: 'INFO', 30: 'WARNING', 40: 'ERROR'}
-		logger.debug('Loglevel set to ' + logLevel[logging.getLogger().getEffectiveLevel()])
-				
-	 
-		##Apply the config, then if arguments, override the config values with args
-		config = read_config(config_file_path)
+	log_level = logging.DEBUG if args.debug else logging.INFO
+	init_logging(log_level,log_file_path)
+	
+	logger.debug("%s v%s is starting up" % (__file__, softwareversion))
+	logLevel = {0: 'NOTSET', 10: 'DEBUG', 20: 'INFO', 30: 'WARNING', 40: 'ERROR'}
+	logger.debug('Loglevel set to ' + logLevel[logging.getLogger().getEffectiveLevel()])
+	
+	
+	##Apply the config, then if arguments, override the config values with args
+	config = read_config(config_file_path)
+	
+	##Print verions
+	if args.version:
+		print ("Monitor Version: %s, Class version:%s" % (softwareversion,ac_db_version.version))
+		sys.exit()
+	
+	##Mqtt Host
+	if args.mqttserver:
+		config["mqtt_host"] = args.mqttserver
 		
-		##Print verions
-		if args.version:
-			print ("Monitor Version: %s, Class version:%s" % (softwareversion,ac_db_version.version))
+	##Mqtt Port
+	if args.mqttport:
+		config["mqtt_port"] = args.mqttport
+	##Mqtt User
+	if args.mqttuser:
+		config["mqtt_user"] = args.mqttuser
+	
+	##Mqtt Password
+	if args.mqttpassword:
+		config["mqtt_password"] = args.mqttpassword
+		
+	##Mqtt auto discovery topic
+	if args.mqtt_auto_discovery_topic:
+		config["mqtt_auto_discovery_topic"] = args.mqtt_auto_discovery_topic
+		
+	
+	##Self Discovery
+	if args.discover:
+		config["self_discovery"] = True			
+	
+	if args.discoverdump:
+		discover_and_dump_for_config(config)
+		
+	##Deamon Mode
+	if args.background:
+		config["daemon_mode"] = True			 
+	
+	##mmmm.. this looks dodgy.. but i'm not python expert 			
+	AC = AcToMqtt.AcToMqtt(config)
+	
+	try:				
+		
+		logging.info("Starting Monitor...")
+		# Start and run the mainloop
+		logger.debug("Starting mainloop, responding on only events")
+	
+		##Connect to Mqtt
+		AC.connect_mqtt()				
+		
+		if config["self_discovery"]:	
+			devices = AC.discover()
+		else:
+			devices = AC.make_device_objects(config['devices'])
+		
+		
+		if args.dumphaconfig:
+			AC.dump_homeassistant_config_from_devices(devices)			
 			sys.exit()
 		
-		##Mqtt Host
-		if args.mqttserver:
-			config["mqtt_host"] = args.mqttserver
-			
-		##Mqtt Port
-		if args.mqttport:
-			config["mqtt_port"] = args.mqttport
-		##Mqtt User
-		if args.mqttuser:
-			config["mqtt_user"] = args.mqttuser
 		
-		##Mqtt Password
-		if args.mqttpassword:
-			config["mqtt_password"] = args.mqttpassword
 			
-		##Mqtt auto discovery topic
-		if args.mqtt_auto_discovery_topic:
-			config["mqtt_auto_discovery_topic"] = args.mqtt_auto_discovery_topic
-		  
-		
-		##Self Discovery
-		if args.discover:
-			config["self_discovery"] = True			
-	 
-		if args.discoverdump:
-			discover_and_dump_for_config(config)
-			
-		##Deamon Mode
-		if args.background:
-			config["daemon_mode"] = True			 
-		
-		##mmmm.. this looks dodgy.. but i'm not python expert 			
-		AC = AcToMqtt.AcToMqtt(config)
-		
-		try:				
-			
-			logging.info("Starting Monitor...")
-			# Start and run the mainloop
-			logger.debug("Starting mainloop, responding on only events")
-		
-			##Connect to Mqtt
-			AC.connect_mqtt()				
-			
-			if config["self_discovery"]:	
-				devices = AC.discover()
-			else:
-				devices = AC.make_device_objects(config['devices'])
-			
-			
-			if args.dumphaconfig:
-				AC.dump_homeassistant_config_from_devices(devices)			
-				sys.exit()
-			
-			
+		##Publish mqtt auto discovery if topic  set
+		if config["mqtt_auto_discovery_topic"]:
+			AC.publish_mqtt_auto_discovery(devices)			
+	
+		##One loop
+		do_loop = True if config["daemon_mode"] else False			
 				
- 			##Publish mqtt auto discovery if topic  set
-			if config["mqtt_auto_discovery_topic"]:
-				AC.publish_mqtt_auto_discovery(devices)			
+		##Run main loop
+		while do_loop :
+			running = True
+			AC.start(config,devices)
+			touch_pid_file()
 		
-			##One loop
-			do_loop = True if config["daemon_mode"] else False			
-					
-			##Run main loop
-			while do_loop :
-				running = True
-				AC.start(config,devices)
-				touch_pid_file()
+		running = False
+	except KeyboardInterrupt:
+		logging.debug("User Keyboard interuped")	
+		
+	except Exception as e:								
+		
+		logger.debug(traceback.format_exc())			
+		logger.error(e)
+		
 			
-			running = False
-		except KeyboardInterrupt:
-			logging.debug("User Keyboard interuped")	
-			
-		except Exception as e:								
-			
-			logger.debug(traceback.format_exc())			
-			logger.error(e)
-			
-				
-		finally:
-			##cleanup
-			stop()
+	finally:
+		##cleanup
+		stop()
 			
 
 				
