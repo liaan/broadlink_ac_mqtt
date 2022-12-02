e420e0d194bbd6e632e583d7c9796150505ba74b
Karman de Lange <you@example.com>
2022-12-02 06:11:36 +0000

    Fix new python

--- monitor.py
+++ monitor.py
@@ -185,17 +185,6 @@ def restart(signalNumber = 0, frame = 0):
 	""
 
 def init_signal():
-	signal.signal(signal.SIGBREAK, restart)
-	signal.signal(signal.SIGINT, receiveSignal)
-	#signal.signal(signal., receiveSignal)
-	signal.signal(signal.SIGILL, receiveSignal)
-	#signal.signal(signal.SIGTRAP, receiveSignal)
-	signal.signal(signal.SIGABRT, receiveSignal)
-	#signal.signal(signal.SIGBUS, receiveSignal)
-	signal.signal(signal.SIGFPE, receiveSignal)
-	#signal.signal(signal.SIGKILL, receiveSignal)
-	#signal.signal(signal.SIGUSR1, receiveSignal)
-	signal.signal(signal.SIGSEGV, receiveSignal)
 #	signal.signal(signal.SIGUSR2, receiveSignal)
 	#signal.signal(signal.SIGPIPE, receiveSignal)
 	#signal.signal(signal.SIGALRM, receiveSignal)
